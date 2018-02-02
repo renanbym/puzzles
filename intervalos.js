8a7a7585d9d7b310cf55b7526bc158e52d09c66a
Renan Mariano <renanbym@gmail.com>
2018-02-01 23:27:17 -0200

    intervalos

--- intervalos.js
+++ intervalos.js
@@ -5,3 +5,24 @@
 // Entrada: 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150
 //
 // Saída: [100-105], [110-111], [113-115], [150]
+
+
+let core = [100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150]
+
+
+function problema(array, res = [] ){
+
+    return array.sort().reduce( (acc, v, i , arr) => {
+
+        if( acc.length > 0 && acc[acc.length-1]+1 !== v ){
+            let n = acc.splice(0, acc.length)
+            res.push([n[0]+"-"+n[n.length-1]])
+        }
+        acc.push( v )
+
+        return acc
+    },[]).concat(res).sort()
+
+    // return res
+}
+console.log( 'result:', problema( core ) );
